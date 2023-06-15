c3fbb3aba260666eb218c313f7c31cbf8c2b0821
tabi5 <tinbitdemissie@gmail.com>
2023-06-15 08:42:22 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - This program prints a line using the printf function
-*
-* Return: 0
-*/
+ * main - This program prints a line using printf function and  without puts
+ * Return: 0
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
